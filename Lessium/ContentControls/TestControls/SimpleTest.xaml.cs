39e13080a513ad8c40c259b6e610bac44f283fd3
Devbeerov <Devbeerov@yandex.ru>
2021-06-17 19:31:19 +0500

    TODO: SimpleTest DataTemplate fix TextControl removeButtonPresenter

--- Lessium/ContentControls/TestControls/SimpleTest.xaml.cs
+++ Lessium/ContentControls/TestControls/SimpleTest.xaml.cs
@@ -106,127 +106,6 @@ namespace Lessium.ContentControls.TestControls
 
         #region Private
 
-        private ContentPresenter GetLastContainer()
-        {
-            // No items, no problems.
-
-            if (AnswersItemControl.Items.Count == 0) return null;
-
-            var lastIndex = AnswersItemControl.Items.Count - 1;
-            var last = AnswersItemControl.Items.GetItemAt(lastIndex);
-
-            return AnswersItemControl.ItemContainerGenerator.ContainerFromItem(last) as ContentPresenter;
-        }
-
-        private TextControl GetTextContainer(ContentPresenter contentPresenter)
-        {
-            // If container does not exist yet, returns.
-            if (contentPresenter == null) return null;
-
-            var dataTemplate = contentPresenter.ContentTemplate;
-
-            return dataTemplate.FindName("TextContainer", contentPresenter) as TextControl;
-        }
-
-        private int GetMaxLineCount(TextBox textBox, double? lineHeight)
-        {
-            if (!lineHeight.HasValue) lineHeight = textBox.CalculateLineHeight();
-
-            var pos = ContentPageControlService.TranslatePoint(this);
-
-            return Convert.ToInt32(Math.Floor((ContentPageControlService.MaxHeight - pos.Y) / lineHeight.Value));
-        }
-
-        private void ValidateAnswersControl(double? lineHeight, SizeChangedEventArgs e)
-        {
-            var lastContainer = GetLastContainer();
-            var textContainer = GetTextContainer(lastContainer);
-
-            if (textContainer == null || ContentPageControlService.IsControlFits(this)) return;
-
-            var textBox = textContainer.textBox;
-            var maxLineCount = GetMaxLineCount(textBox, lineHeight);
-
-            if (textBox.LineCount <= maxLineCount) return;
-
-            var prevCaret = textBox.CaretIndex; // Caret before removing everything past MaxLine
-            var lastPositionInMaxLine = CalculateLastPositionInMaxLine(textBox, maxLineCount);
-
-            // Removes all excessing and creates new text.
-
-            var newText = CreateNewText(textContainer, lastPositionInMaxLine);
-
-            UpdateTextContainer(textContainer, newText);
-            UpdateBorder();
-            UpdateCaret(textBox, prevCaret, newText.Length);
-
-            e.Handled = true;
-        }
-
-        private static int CalculateLastPositionInMaxLine(TextBox textBox, int maxLineCount)
-        {
-            int MaxLineIndex = maxLineCount - 1;
-            int firstPositionInMaxLine = textBox.GetCharacterIndexFromLineIndex(MaxLineIndex);
-            int lengthOfMaxLine = textBox.GetLineLength(MaxLineIndex);
-
-            return firstPositionInMaxLine + lengthOfMaxLine;
-        }
-
-        private string CreateNewText(TextControl textContainer, int lastPositionInMaxLine)
-        {
-            var actualText = textContainer.Text;
-
-            return actualText.Remove(lastPositionInMaxLine - 1);
-        }
-
-        private static void UpdateCaret(TextBox textBox, int prevCaret, int newLength)
-        {
-            // Restores caret
-
-            if (prevCaret > newLength)
-            {
-                prevCaret = newLength - 1;
-            }
-
-            textBox.CaretIndex = prevCaret;
-        }
-
-        private void UpdateBorder()
-        {
-            border.InvalidateMeasure();
-            border.UpdateLayout();
-        }
-
-        private static void UpdateTextContainer(TextControl textContainer, string newText)
-        {
-            textContainer.Text = newText;
-            textContainer.InvalidateMeasure();
-            textContainer.UpdateLayout();
-        }
-
-        private double CalculateAnswersControlMaxHeight()
-        {
-            double toSubstract = 0d;
-
-            for (int i = 0; i < AnswersItemControl.Items.Count; i++)
-            {
-                var item = AnswersItemControl.Items.GetItemAt(i);
-                var itemContainer = AnswersItemControl.ItemContainerGenerator.ContainerFromItem(item) as ContentPresenter;
-                toSubstract += itemContainer.ActualHeight;
-            }
-
-            return MaxHeight - toSubstract; // new maxHeight
-        }
-
-        private void UpdateAnswersControlMaxHeight(double? lineHeight)
-        {
-            var maxHeight = CalculateAnswersControlMaxHeight();
-
-            if (!lineHeight.HasValue) lineHeight = testQuestion.textBox.CalculateLineHeight();
-
-            testQuestion.textBox.MaxLines = Convert.ToInt32(Math.Floor(maxHeight / lineHeight.Value));
-        }
-
         private DynamicCheckBoxType GetActualCheckBoxType()
         {
             if (IsEditable) { return DynamicCheckBoxType.CheckBox; }
@@ -241,10 +120,9 @@ namespace Lessium.ContentControls.TestControls
             CheckBoxType = GetActualCheckBoxType();
         }
 
-        private SimpleAnswerModel FindAnswerModelFromSenderControl(object sender)
+        private SimpleAnswerModel FindAnswerModelFromElement(FrameworkElement element)
         {
-            var senderControl = sender as Control;
-            var answerControlContainer = senderControl.FindParentOfName("dataPanel");
+            var answerControlContainer = element.FindParentOfName("dataPanel");
 
             // if dataPanel not found it means either it was because DynamicCheckBoxType was changed
             if (answerControlContainer == null) throw new NullReferenceException("Failed to find parent with name \"dataPanel\".");
@@ -254,23 +132,8 @@ namespace Lessium.ContentControls.TestControls
 
         private void UpdateCheckboxes()
         {
-            UpdateCheckboxes(TrueAnswers, mappingHelper.GetCheckBoxTypes(TrueAnswers));
-            UpdateCheckboxes(SelectedAnswers, mappingHelper.GetCheckBoxTypes(SelectedAnswers));
-        }
-
-        private void UpdateCheckboxes(IList<SimpleAnswerModel> answers, List<DynamicCheckBoxType> checkBoxTypes)
-        {
-            foreach (var item in answers)
-            {
-                var containerPresenter = AnswersItemControl.ItemContainerGenerator.ContainerFromItem(item) as ContentPresenter;
-                var contentTemplate = containerPresenter.ContentTemplate;
-                var checkBox = contentTemplate.FindName("checkBox", containerPresenter) as DynamicCheckBox;
-
-                foreach (var checkBoxType in checkBoxTypes)
-                {
-                    checkBox.UpdateIsChecked(true, checkBoxType);
-                }
-            }
+            AnswersItemControl.UpdateCheckboxes(mappingHelper.GetCheckBoxTypes(TrueAnswers));
+            AnswersItemControl.UpdateCheckboxes(mappingHelper.GetCheckBoxTypes(SelectedAnswers));
         }
 
         private void UpdateCheckboxesOnceAtLoad()
@@ -313,7 +176,6 @@ namespace Lessium.ContentControls.TestControls
         {
             var control = sender as SimpleTest;
             var addAnswerButton = control.addAnswerButton;
-            var AnswersItemControl = control.AnswersItemControl;
             var IsEditable = (bool)args.NewValue;
 
             control.ValidateAnswersSelectionMode();
@@ -322,21 +184,6 @@ namespace Lessium.ContentControls.TestControls
 
             addAnswerButton.IsEnabled = IsEditable;
             addAnswerButton.Visibility = IsEditable ? Visibility.Visible : Visibility.Collapsed;
-
-            // Answers Controls
-
-            for (int i = 0; i < AnswersItemControl.Items.Count; i++)
-            {
-                // Should be used ContainerFromIndex instead of ContainerFromItem,
-                // Because it will return same ContentPresenter for each element due to overriden AnswerMode.Equals (perhaps GetHashCode involved too)
-                var contentPresenter = AnswersItemControl.ItemContainerGenerator.ContainerFromIndex(i) as ContentPresenter;
-                if (contentPresenter != null) // If container is already loaded, otherwise we wait for TextContainer_Loaded method
-                {
-                    DataTemplate dataTemplate = contentPresenter.ContentTemplate;
-                    TextControl textContainer = dataTemplate.FindName("TextContainer", contentPresenter) as TextControl;
-                    textContainer.IsEditable = IsEditable;
-                }
-            }
         }
 
         #endregion
@@ -356,22 +203,14 @@ namespace Lessium.ContentControls.TestControls
 
         #region Events
 
-        private void Question_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            // Sets ItemControl MaxHeight to parent border (holder of Question and ItemsControl) MaxHeight minus NewSize.
-            AnswersItemControl.MaxHeight = border.MaxHeight - e.NewSize.Height;
-        }
-
         private void AnswersItemControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             if (!raiseResizeEvent) { return; }
 
-            double? lineHeight = new double?();
-
             if (addAnswerButton.Visibility != Visibility.Collapsed)
             {
                 var buttonPos = addAnswerButton.TranslatePoint(default, AnswersItemControl);
-                lineHeight = testQuestion.textBox.CalculateLineHeight();
+                var lineHeight = testQuestion.textBox.CalculateLineHeight();
                 var maximumValidPosY = buttonPos.Y + addAnswerButton.ActualHeight + lineHeight;
 
                 var show = buttonPos.Y <= maximumValidPosY;
@@ -384,18 +223,9 @@ namespace Lessium.ContentControls.TestControls
                 if (!show) return; 
             }
 
-            // Performs all validation and fixing, could also set e.Handled = true, to prevent multiple event calls.
-
-            ValidateAnswersControl(lineHeight, e);
-
-            // Updates AnswersControl.MaxHeight properly with calculations.
-
-            //UpdateAnswersControlMaxHeight(lineHeight);
-
             // Sets source to SimpleTest Control, not Border
 
             e.Source = this;
-
             raiseResizeEvent = true;
         }
 
@@ -439,7 +269,7 @@ namespace Lessium.ContentControls.TestControls
 
         private void ToggleAnswerTrue_Checked(object sender, RoutedEventArgs e)
         {
-            var answerModel = FindAnswerModelFromSenderControl(sender);
+            var answerModel = FindAnswerModelFromElement(sender as FrameworkElement);
 
             TrueAnswers.Add(answerModel);
 
@@ -448,7 +278,7 @@ namespace Lessium.ContentControls.TestControls
 
         private void ToggleAnswerTrue_Unchecked(object sender, RoutedEventArgs e)
         {
-            var answerModel = FindAnswerModelFromSenderControl(sender);
+            var answerModel = FindAnswerModelFromElement(sender as FrameworkElement);
 
             TrueAnswers.Remove(answerModel);
 
@@ -457,14 +287,14 @@ namespace Lessium.ContentControls.TestControls
 
         private void AnswerSelected(object sender, RoutedEventArgs e)
         {
-            var answerModel = FindAnswerModelFromSenderControl(sender);
+            var answerModel = FindAnswerModelFromElement(sender as FrameworkElement);
 
             SelectedAnswers.Add(answerModel);
         }
 
         private void AnswerUnselected(object sender, RoutedEventArgs e)
         {
-            var answerModel = FindAnswerModelFromSenderControl(sender);
+            var answerModel = FindAnswerModelFromElement(sender as FrameworkElement);
 
             SelectedAnswers.Remove(answerModel);
         }
---
+++ Lessium/CustomControls/PageItemsControl.xaml.cs
@@ -0,0 +1,181 @@
+﻿using Lessium.ContentControls.MaterialControls;
+using Lessium.Converters;
+using Lessium.Services;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Lessium.CustomControls
+{
+    /// <summary>
+    /// Manages Items ContentPresenters MaxHeight property to ensure that everything will have proper.
+    /// Have built-in support for TextControl.
+    /// However, each Item that want this behavior to work, should bind it's MaxHeight property to ContentPresenter.
+    /// Example: {Binding RelativeSource={RelativeSource Mode=TemplatedParent}, Path=MaxHeight}
+    /// </summary>
+    public partial class PageItemsControl : UserControl
+    {
+        private static UIElementsDistanceConverter distanceConverter;
+
+        public PageItemsControl()
+        {
+            WeakEventManager<PageItemsControl, RoutedEventArgs>.AddHandler(this, nameof(Loaded), OnLoaded);
+            InitializeComponent();
+        }
+
+        #region Methods
+
+        private UIElementsDistanceConverter FindDistanceConverter()
+        {
+            var converter = (UIElementsDistanceConverter)Application.Current.FindResource(nameof(UIElementsDistanceConverter));
+
+            if (converter == null) throw new KeyNotFoundException("Failed to find converter.");
+
+            return converter;
+        }
+
+        private double CalculateItemMaxHeight(FrameworkElement item, FrameworkElement nextItem)
+        {
+            if (distanceConverter == null) distanceConverter = FindDistanceConverter();
+
+            double maxItemHeight = MaxHeight;
+
+            var inputToItemsControl = new object[] { this, item };
+            var distanceToItemsControl = (double)distanceConverter.Convert(inputToItemsControl, null, Coordinate.Y.ToString(), CultureInfo.InvariantCulture);
+
+            maxItemHeight -= distanceToItemsControl;
+
+            if (nextItem != null)
+            {
+                var inputToNextItem = new object[] { item, nextItem };
+                var distanceToNextItem = (double)distanceConverter.Convert(inputToNextItem, null, Coordinate.Y.ToString(), CultureInfo.InvariantCulture);
+
+                maxItemHeight -= distanceToNextItem;
+            }
+
+            return maxItemHeight;
+        }
+
+        private void UpdateItemsMaxHeight()
+        {
+            // Backwards
+            for (int i = innerItemsControl.Items.Count - 1; i >= 0; i--)
+            {
+                var itemPresenter = (ContentPresenter)innerItemsControl.ItemContainerGenerator.ContainerFromIndex(i);
+                ContentPresenter nextItemPresenter = null;
+
+                if (i + 1 != innerItemsControl.Items.Count)
+                    nextItemPresenter = (ContentPresenter)innerItemsControl.ItemContainerGenerator.ContainerFromIndex(i + 1);
+
+                var maxHeight = CalculateItemMaxHeight(itemPresenter, nextItemPresenter);
+
+                itemPresenter.MaxHeight = maxHeight;
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+            WeakEventManager<PageItemsControl, RoutedEventArgs>.RemoveHandler(this, nameof(Loaded), OnLoaded);
+
+            UpdateItemsMaxHeight();
+        }
+
+        private void innerItemsControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateItemsMaxHeight();
+        }
+
+        #endregion
+
+        #region Dependency Properties
+
+        public bool IsEditable
+        {
+            get { return (bool)GetValue(IsEditableProperty); }
+            set { SetValue(IsEditableProperty, value); }
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsEditableProperty =
+            DependencyProperty.Register("IsEditable", typeof(bool), typeof(PageItemsControl), new PropertyMetadata(false,
+                new PropertyChangedCallback(OnEditableChanged)));
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(PageItemsControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(PageItemsControl), new PropertyMetadata(null));
+
+        private static void OnEditableChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var pageItemControl = sender as PageItemsControl;
+            var innerItemsControl = pageItemControl.innerItemsControl;
+
+            for (int i = 0; i < innerItemsControl.Items.Count; i++)
+            {
+                // Should be used ContainerFromIndex instead of ContainerFromItem,
+                // Because it will return same ContentPresenter for each element due to overriden AnswerMode.Equals (perhaps GetHashCode involved too)
+                var contentPresenter = innerItemsControl.ItemContainerGenerator.ContainerFromIndex(i) as ContentPresenter;
+                if (contentPresenter != null) // If container is already loaded, otherwise we wait for TextContainer_Loaded method
+                {
+                    var dataTemplate = contentPresenter.ContentTemplate;
+                    var textContainer = dataTemplate.FindName("TextContainer", contentPresenter) as TextControl;
+
+                    // Support for TextContainer
+                    if (textContainer != null) textContainer.IsEditable = pageItemControl.IsEditable;
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// It's just very simple implementation of methods, that are should not be directly presented in PageItemsControl.
+    /// Because it's responsibility just to manage Items properly to fit the Page.
+    /// Also possible to make PageItemsControl abstract, but it's not needed for now.
+    /// </summary>
+    public static class PageItemControlExtensions
+    {
+        /// <summary>
+        /// Updates all DynamicCheckBox that are Items of PageItemsControl.
+        /// </summary>
+        /// <param name="checkBoxTypes"></param>
+        public static void UpdateCheckboxes(this PageItemsControl control, List<DynamicCheckBoxType> checkBoxTypes)
+        {
+            var innerItemsControl = control.innerItemsControl;
+
+            foreach (var item in innerItemsControl.Items)
+            {
+                var containerPresenter = innerItemsControl.ItemContainerGenerator.ContainerFromItem(item) as ContentPresenter;
+                var contentTemplate = containerPresenter.ContentTemplate;
+                var checkBox = contentTemplate.FindName("checkBox", containerPresenter) as DynamicCheckBox;
+
+                if (checkBox == null) continue;
+
+                foreach (var checkBoxType in checkBoxTypes)
+                {
+                    checkBox.UpdateIsChecked(true, checkBoxType);
+                }
+            }
+        }
+    }
+}
